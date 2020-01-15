17373cb0b2c1abb55a6c01092243f3847028defa
ddnguyen <duy.dang.nguyen@mgm-tp.com>
2020-01-15 16:41:08 +0700

    basic impl for controller

---
+++ api/src/main/kotlin/com/ongdev/blog/api/configs/WebConfiguration.kt
@@ -0,0 +1,7 @@
+package com.ongdev.blog.api.configs
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class WebConfiguration {
+}
---
+++ api/src/main/kotlin/com/ongdev/blog/api/models/dtos/ArticleDTO.kt
@@ -0,0 +1,3 @@
+package com.ongdev.blog.api.models.dtos
+
+class ArticleDTO(val it: String, val title: String, val description: String)
