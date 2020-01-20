2eea580a52a1b890c83e461041acdee0c55d5299
MiloNguyen95 <milonguyen95@outlook.com>
2020-01-20 17:50:55 +0700

    Add services to get all and getWithPagination

--- api/src/main/kotlin/com/ongdev/blog/api/models/dtos/requests/ArticleCreationRequest.kt
+++ api/src/main/kotlin/com/ongdev/blog/api/models/dtos/requests/ArticleCreationRequest.kt
@@ -1,3 +1,13 @@
 package com.ongdev.blog.api.models.dtos.requests
 
-class ArticleCreationRequest(val it: String, val title: String, val description: String, val authorId: String)
+import java.util.*
+
+class ArticleCreationRequest(
+	val title: String,
+    val description: String,
+    val content: String,
+    val name: String,
+    val publishDate: Date?,
+    val link: String,
+	val authorId: String
+)
--- api/src/main/kotlin/com/ongdev/blog/api/models/dtos/responses/ArticleCreationResponse.kt
+++ api/src/main/kotlin/com/ongdev/blog/api/models/dtos/responses/ArticleCreationResponse.kt
@@ -1,3 +1,14 @@
 package com.ongdev.blog.api.models.dtos.responses
 
-class ArticleCreationResponse(val id: String, val title: String, val description: String, val authorId: String)
+import java.util.*
+
+class ArticleCreationResponse(
+	val id: String,
+    val title: String,
+    val description: String,
+    val content: String,
+    val name: String,
+    val publishDate: Date?,
+    val link: String,
+	val authorId: String
+)
