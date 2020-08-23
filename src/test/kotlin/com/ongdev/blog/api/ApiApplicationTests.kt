af6c3f4bca9d60b6c0ee921a708738f2bd05bcab
Duy Nguyen <milonguyen95@outlook.com>
2020-08-23 12:49:22 +0700

    Refactor, use Github Actions

---
+++ src/test/kotlin/com/ongdev/blog/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ongdev.blog.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
