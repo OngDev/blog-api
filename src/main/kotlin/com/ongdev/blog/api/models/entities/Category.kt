436dd53bc14604114f1c52685ef0449131e69c5b
vkhanhqui <vkhanhqui@gmail.com>
2020-08-24 21:04:39 +0700

    delete fetch

--- src/main/kotlin/com/ongdev/blog/api/models/entities/Category.kt
+++ src/main/kotlin/com/ongdev/blog/api/models/entities/Category.kt
@@ -7,7 +7,7 @@ import javax.persistence.*
 data class Category(
         var name: String = "",
         var link: String = "", // remove letter accent and replace space with dash '-' from name
-        @ManyToMany(cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+        @ManyToMany
 
         @JoinTable(name = "article_category",
                 joinColumns = [JoinColumn(name = "category_id", referencedColumnName = "id")],
