19fa6e3e3fda2cc98b5929ae0d476d345d09c5ed
Khanh Qui <vkhanhqui@gmail.com>
2020-10-01 20:02:56 +0700

    add bad test case of service

--- src/test/kotlin/com/ongdev/blog/api/services/ArticleServiceTests.kt
+++ src/test/kotlin/com/ongdev/blog/api/services/ArticleServiceTests.kt
@@ -1,5 +1,6 @@
 package com.ongdev.blog.api.services
 
+import com.ongdev.blog.api.exceptions.ListArticlesNotFoundException
 import com.ongdev.blog.api.models.entities.Article
 import com.ongdev.blog.api.models.entities.Author
 import com.ongdev.blog.api.models.entities.Category
@@ -8,11 +9,9 @@ import com.ongdev.blog.api.services.impl.ArticleServiceImpl
 import com.ongdev.blog.api.services.interfaces.ArticleService
 import junit.framework.Assert.assertEquals
 import org.junit.Test
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.runner.RunWith
 import org.mockito.Mockito
-import org.mockito.junit.MockitoJUnitRunner
-import org.springframework.beans.support.PagedListHolder
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
@@ -29,25 +28,47 @@ class ArticleServiceTests {
     private lateinit var articleService: ArticleService
 
     @Test
-    fun testGetListArticlesByCategory() {
-        val category = Category("","", emptySet())
-        category.id= UUID.randomUUID()
-        val tempCategoryId=category.id.toString()
-        val setCategory= setOf(category)
-        val listArticle= ArrayList<Article>()
-        for(i in 0..9){
-            listArticle.add(i, Article("","","","",null
+    fun testGetListArticlesByCategorySuccessfully() {
+        val category = Category("", "", emptySet())
+        category.id = UUID.randomUUID()
+        val tempCategoryId = category.id.toString()
+        val setCategory = setOf(category)
+        val listArticle = ArrayList<Article>()
+        for (i in 0..9) {
+            listArticle.add(i, Article("", "", "", "", null
                     , Author(), emptySet(), setCategory, emptySet()))
         }
         val pageable: Pageable = PageRequest.of(0, 10)
-        val pagingByCategory:Page<Article> = PageImpl<Article>(listArticle,pageable,1)
+        val pagingByCategory: Page<Article> = PageImpl<Article>(listArticle, pageable, 1)
         val toOptional = Optional.of(pagingByCategory)
         Mockito.`when`(articleRepository
-                .findAllArticlesByCategoryId(UUID.fromString(tempCategoryId),pageable))
+                .findAllArticlesByCategoryId(UUID.fromString(tempCategoryId), pageable))
                 .thenReturn(toOptional)
         articleService = ArticleServiceImpl(articleRepository)
-        assertEquals("Number of stories should be 10",10
-                ,articleService.getListArticlesByCategory(tempCategoryId,pageable).result.totalElements)
+        assertEquals("Number of stories should be 10", 10
+                , articleService.getListArticlesByCategory(tempCategoryId, pageable).result.totalElements)
+    }
+
+    @Test
+    fun testGetListArticlesByCategoryBadly() {
+        val category = Category("", "", emptySet())
+        category.id = UUID.randomUUID()
+        val setCategory = setOf(category)
+        val listArticle = ArrayList<Article>()
+        for (i in 0..9) {
+            listArticle.add(i, Article("", "", "", "", null
+                    , Author(), emptySet(), setCategory, emptySet()))
+        }
+        val pageable: Pageable = PageRequest.of(0, 10)
+        val pagingByCategory: Page<Article> = PageImpl<Article>(listArticle, pageable, 1)
+        val toOptional = Optional.of(pagingByCategory)
+        Mockito.`when`(articleRepository
+                .findAllArticlesByCategoryId(UUID.fromString(category.id.toString()), pageable))
+                .thenReturn(toOptional)
+        articleService = ArticleServiceImpl(articleRepository)
+        Assertions.assertThrows(ListArticlesNotFoundException::class.java) {
+            articleService.getListArticlesByCategory(UUID.randomUUID().toString(), pageable)
+        }
     }
 
 }
