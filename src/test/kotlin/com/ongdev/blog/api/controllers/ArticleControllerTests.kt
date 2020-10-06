8f3d24c29ababe2e803e28e1b788b9194cff646e
Khanh Qui <vkhanhqui@gmail.com>
2020-10-06 21:36:52 +0700

    <bug> mock articleController

--- src/test/kotlin/com/ongdev/blog/api/controllers/ArticleControllerTests.kt
+++ src/test/kotlin/com/ongdev/blog/api/controllers/ArticleControllerTests.kt
@@ -6,30 +6,31 @@ import com.ongdev.blog.api.models.entities.Author
 import com.ongdev.blog.api.models.entities.Category
 import com.ongdev.blog.api.models.toArticleCreationResponse
 import com.ongdev.blog.api.services.interfaces.ArticleService
-import junit.framework.Assert.assertEquals
 import org.junit.Test
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
-import org.springframework.http.HttpStatus
-import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import java.util.*
 import kotlin.collections.ArrayList
 
-@ExtendWith(SpringExtension::class)
-class ArticleControllerTests {
 
-    private val articleService: ArticleService = Mockito.mock(ArticleService::class.java)
+@WebMvcTest
+class ArticleControllerTests(@Autowired val mockMvc: MockMvc) {
 
-    private lateinit var articleController: ArticleController
+    @MockBean
+    private lateinit var articleService: ArticleService
 
     @Test
-    fun testGetListArticlesForEachCategorySuccessfully() {
+    fun `test Get List Articles By Category, Successfully`() {
         val category = Category("", "", emptySet())
         category.id = UUID.randomUUID()
         val tempCategoryId = category.id.toString()
@@ -44,19 +45,13 @@ class ArticleControllerTests {
         val response = ArticleListWithPaginationResponse(pagingByCategory.map {
             it.toArticleCreationResponse()
         })
+
         Mockito.`when`(articleService
                 .getListArticlesByCategory(tempCategoryId, pageable))
                 .thenReturn(response)
-        articleController = ArticleController(articleService)
-        val result = articleController.getListArticlesForEachCategory(tempCategoryId,pageable)
-        assertEquals(HttpStatus.OK, result.statusCode)
-        assertEquals(response, result.body)
-    }
 
-    @Test
-    fun testGetListArticlesForEachCategoryBadly() {
-        articleController = ArticleController(articleService)
-        val result = articleController.getListArticlesForEachCategory(UUID.randomUUID().toString(),PageRequest.of(0, 10))
-        assertEquals(null, result.body)
+        mockMvc.perform(MockMvcRequestBuilders.get("/articles/category/$tempCategoryId").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
     }
 }
