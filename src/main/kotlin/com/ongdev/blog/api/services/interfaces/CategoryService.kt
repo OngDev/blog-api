962583f44284caa4c1544fc729f9487649df02b7
Khanh Qui <vkhanhqui@gmail.com>
2021-01-01 13:58:23 +0700

    add conntroller and service

--- src/main/kotlin/com/ongdev/blog/api/services/interfaces/CategoryService.kt
+++ src/main/kotlin/com/ongdev/blog/api/services/interfaces/CategoryService.kt
@@ -2,14 +2,15 @@ package com.ongdev.blog.api.services.interfaces
 
 import com.ongdev.blog.api.models.dtos.requests.CategoryCreationRequest
 import com.ongdev.blog.api.models.dtos.requests.CategoryUpdatingRequest
-import com.ongdev.blog.api.models.dtos.responses.CategoryResponse
 import com.ongdev.blog.api.models.dtos.responses.CategoriesWithPaginationResponse
+import com.ongdev.blog.api.models.dtos.responses.CategoryResponse
 import org.springframework.data.domain.Pageable
 
 interface CategoryService {
-    fun getCategoryById(id: String): CategoryResponse
     fun getAllCategories(pageable: Pageable): CategoriesWithPaginationResponse
     fun createCategory(categoryCreationRequest: CategoryCreationRequest): CategoryResponse
+    fun getCategoriesByLink(link: String, pageable: Pageable): CategoriesWithPaginationResponse
+    fun getCategoryById(id: String): CategoryResponse
     fun updateCategoryById(categoryUpdatingRequest: CategoryUpdatingRequest, id: String): CategoryResponse
     fun deleteCategoryById(id: String)
 }
